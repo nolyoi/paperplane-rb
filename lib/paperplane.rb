ed01b2e9ccd0c2d828414e607d818c3070ab5d70
nolyoi <nolan@syslogica.io>
2023-03-28 01:34:33 -0500

    Initial commit.

---
+++ lib/paperplane.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative "paperplane/version"
+require 'http'
+
+module Paperplane
+  class Error < StandardError; end
+
+  class << self
+    BASE_URL = 'https://api.paperplane.app/v1'.freeze
+    ENDPOINTS = {
+      create_job: '/jobs',
+      show_job: '/jobs/%{id}',
+      download_pdf: 'https://download.paperplane.app/'
+    }.freeze
+    PAGE_SIZES = %w[A4 Letter Legal Tabloid].freeze
+
+    attr_accessor :api_key
+
+    def create_job(url, page_size = 'A4')
+      validate_page_size(page_size)
+      perform_request(:post, :create_job, url, page_size:)
+    end
+
+    def show_job(id)
+      perform_request(:get, :show_job, id)
+    end
+
+    def download_pdf(url, page_size = 'A4')
+      validate_page_size(page_size)
+      perform_request(:post, :download_pdf, url, page_size:)
+    end
+
+    private
+
+    def http_client
+      @http_client ||= HTTP.basic_auth(user: @api_key, pass: '')
+    end
+
+    def perform_request(method, endpoint_name, *args)
+      url = build_url(endpoint_name, args[0])
+      response = http_client.request(method, url, json: args[1])
+      handle_error(response)
+      response.parse
+    end
+
+    def build_url(endpoint_name, id)
+      endpoint = ENDPOINTS[endpoint_name]
+      endpoint.include?('://') ? endpoint : "#{BASE_URL}#{format(endpoint, id:)}"
+    end
+
+    def handle_error(response)
+      raise Paperplane::Error, response.parse['message'] if response.status >= 400
+    end
+
+    def validate_page_size(page_size)
+      raise ArgumentError, "Invalid page size '#{page_size}'" unless PAGE_SIZES.include?(page_size)
+    end
+  end
+end
